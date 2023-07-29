a501c85b763befad98dedc7d148ca64f07b870c1
Z1mme <simon@brakenhielm.com>
2023-07-29 16:51:16 +0200

    BFS

--- Realm Rush Pathfinding/Assets/Tiles/CoordinateLabeler.cs
+++ Realm Rush Pathfinding/Assets/Tiles/CoordinateLabeler.cs
@@ -10,17 +10,18 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.black;
     [SerializeField] Color blockedColor = Color.gray;
-
+    [SerializeField] Color exploredColor = Color.red;
+    [SerializeField] Color pathColor = Color.cyan;
 
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
 
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
 
@@ -47,13 +48,23 @@ public class CoordinateLabeler : MonoBehaviour
 
     void SetLabelColor()
     {
-        if (waypoint.IsPlaceable)
-        {
-            label.color = defaultColor;
-        }
-        else
-        {
+        if(gridManager == null){ return; }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if(node == null){return;}
+
+        if(!node.isWalkable){
             label.color = blockedColor;
+        }
+        else if(node.isPath){
+            label.color = pathColor;
+        }
+        else if(node.isExplored){
+            label.color = exploredColor;
+        }
+        else{
+            label.color = defaultColor;
         }
     }
 
